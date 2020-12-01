74e762d94c4feb734989e4a114ed1e7b5af22122
sskitajima <[id+username]@users.noreply.github.com>
2020-12-01 12:12:27 +0900

    srcにコメントを追加

--- src/SD_sample_camera.cpp
+++ src/SD_sample_camera.cpp
@@ -6,15 +6,21 @@
 
 #include <opencv2/highgui/highgui.hpp>
 
+// 画像トピックを受けたときに実行されるコールバック関数
 void camera_callback(const sensor_msgs::ImageConstPtr& img_msg)
 {
   try
   {
+    // 画像をbgrの8bit形式に変換を試み、それを表示
     cv::imshow("view", cv_bridge::toCvShare(img_msg, "bgr8")->image);
+
+    // 30ms表示
     cv::waitKey(30);
   }
   catch (cv_bridge::Exception& e)
   {
+    // 変換に失敗すると、メッセージを出す
+    // 例:トピックがモノクロ画像だったときなど
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", img_msg->encoding.c_str());
   }
 }
@@ -24,8 +30,14 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "camera_sample_node");
   ros::NodeHandle nh;
+
+  // 画像は直接pub/subするとデータ量が大きいので、image_transportを介して行うのが普通
   image_transport::ImageTransport it(nh);
+
+  // image_transportを介して、画像のトピックのサブスクライバの定義
   image_transport::Subscriber sub = it.subscribe("/camera/rgb/image_color", 1, camera_callback);
+
+  // おまじない
   ros::spin();
 
     return 0;
--- src/SD_sample_laser.cpp
+++ src/SD_sample_laser.cpp
@@ -32,6 +32,7 @@ public:
     pub_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
   }
 
+  // 速度指令値を保持する変数に値を代入する関数
   geometry_msgs::Twist create_vel_msg(const double vel, const double omega)
   {
     // http://docs.ros.org/en/melodic/api/geometry_msgs/html/msg/Twist.html
@@ -42,6 +43,8 @@ public:
     return cmd_msg;
   }
 
+  // laserトピックを受けたときに実行される関数
+  // 最新のスキャン情報を保存しておく
   void laser_callback(const sensor_msgs::LaserScanConstPtr &laser_msg)
   {
     latest_scan = *laser_msg;
